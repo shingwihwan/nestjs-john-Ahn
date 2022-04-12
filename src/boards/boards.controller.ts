70721ad5d2c02941d9180ebcd18e1aedbfeee7cf
zamar <zerll9100@gmail.com>
2022-04-12 23:47:02 +0900

    CRUD 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Board } from './board.moel';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Board, BoardStatus } from './board.moel';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board-dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,9 +14,26 @@ export class BoardsController {
 
     @Post()
     createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
+        @Body() createBoardDto: CreateBoardDto
     ): Board {
-        return this.boardService.createBoard(title, description);
+        return this.boardService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        return this.boardService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoard(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus
+    ) {
+        return this.boardService.updateBoardStatus(id, status);
     }
 }
